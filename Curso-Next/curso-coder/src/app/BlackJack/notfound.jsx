e146b77b868dd2d2175f0eff7e36d3e36b1cca2c
lautaro rodriguez <lautarorodriguezovalle@gmail.com>
2024-01-29 06:38:39 -0300

    Seven Commit

---
+++ Curso-Next/curso-coder/src/app/BlackJack/notfound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter } from 'next/navigation'
+const notfound = () => {
+    const router = useRouter()
+    return (
+        <div className='flex justify-center items-center h-[100vh] flex-col'>
+
+            <p className='my-6'>Pagina No encontrada!!!</p>
+
+            <button className="mx-5 cursor-pointer shadow p-3 rounded-[20px] bg-purple-800" onClick={() => router.back()}>Volver</button>
+        </div>
+    )
+}
+
+export default notfound
